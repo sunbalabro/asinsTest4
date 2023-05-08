3207a16c7e31c709cc249c15634fa4c551ffb1dc
sunbalabro <sunbalabro786@gmail.com>
2023-05-08 16:01:20 +0500

    task complete

---
+++ src/Component/SearchEmoji/index.js
@@ -0,0 +1,65 @@
+import Picker from '@emoji-mart/react'
+import React, { useEffect, useState } from 'react'
+import data from '@emoji-mart/data'
+import { Button, Col, Popover, Row } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+export const SearchEmoji = ({ onEmojiSelect }) => {
+
+  const [emojis, setEmojis] = useState([])
+  const [emojiInput, setEmojiInput] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+  const content = (
+    <div>
+      <p>Copied</p>
+    </div>
+  );
+  const handleChange = (e) => {
+    const sample = e.target.value
+    console.log({ sample })
+    setEmojiInput(sample)
+  }
+  const handleClick = () => {
+    setSearchQuery(emojiInput)
+    setEmojiInput('')
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchApi = await fetch(`https://emojifinder.com/*/ajax.php?action=search&query=${searchQuery}`)
+      const EmojiData = await fetchApi.json()
+      console.log({ EmojiData })
+      setEmojis(EmojiData.results)
+    }
+    fetchData()
+  }, [searchQuery])
+
+  return (
+    <div className='pickerSelection'>
+      <h1>Search Emoji</h1>
+      <div className='h_find'>
+        
+            <input onChange={(e) => handleChange(e)} placeholder='Search Emoji' value={emojiInput} />
+            <button
+              onClick={() => handleClick()}
+            ><FontAwesomeIcon icon={faSearch} />
+            </button>
+
+      </div>
+
+      <br />
+      {
+        emojis ? emojis.map((item) => (
+          <>
+          <Popover content={content} trigger='click'>
+            <span className='emojiSpan' onClick={()=> navigator.clipboard.writeText(String.fromCodePoint(parseInt(item.Code, 16)))}>{String.fromCodePoint(parseInt(item.Code, 16))}</span> &nbsp;
+          </Popover>
+          </>
+        )) : null
+      }
+
+    </div>
+  )
+
+}
